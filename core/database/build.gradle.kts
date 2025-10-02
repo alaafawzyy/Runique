7ee1ff8d5c857894d715b6e132f07274ea7e9125
alaafawzy <160539911+alaafawzyy@users.noreply.github.com>
2025-10-02 23:29:12 +0300

    update library versions and add new dependencies

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -33,11 +33,7 @@ android {
 }
 
 dependencies {
+    implementation(projects.core.domain)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.org.mongodb.bson)
 }
