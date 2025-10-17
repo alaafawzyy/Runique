045deb71eff7e3171e7475e23a1a1bb1ec4cbfda
alaafawzy <alaaf9443@gmail.com>
2025-10-18 00:50:33 +0300

    Update dependencies

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+    `kotlin-dsl`
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+group = "com.plcoding.runique.buildlogic"
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
 }
