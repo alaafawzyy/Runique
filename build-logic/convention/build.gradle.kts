1f07f19f90d2e572d0f7d8c10cd34efd0679336d
alaafawzy <160539911+alaafawzyy@users.noreply.github.com>
2025-10-22 20:55:45 +0300

    Update group ID and add Android application plugin registration

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,12 +1,22 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     `kotlin-dsl`
 }
 
-group = "com.plcoding.runique.buildlogic"
+group = "com.example.runique.buildlogic"
+
 dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.android.tools.common)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
     compileOnly(libs.room.gradlePlugin)
+}
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "example.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
 }
