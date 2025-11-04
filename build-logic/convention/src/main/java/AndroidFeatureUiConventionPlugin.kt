30ff886b19a6427db163f6746b5ae3db9d19bafb
alaafawzy <160539911+alaafawzyy@users.noreply.github.com>
2025-11-04 16:47:32 +0200

    add AndroidFeatureUiConventionPlugin

--- build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
@@ -8,7 +8,7 @@ class AndroidFeatureUiConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.run {
             pluginManager.run {
-                apply("runique.android.library.compose")
+                apply("example.android.library.compose")
             }
 
             dependencies {
